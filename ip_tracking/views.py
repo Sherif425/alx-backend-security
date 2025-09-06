0b5c889f9e4b8a1d68dea7195b3872bc5bd0124f
paygateitteam <73990524+paygateitteam@users.noreply.github.com>
2025-09-06 12:06:29 +0300

    Rate Limiting by IP

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from ratelimit.decorators import ratelimit
+
+# 5 req/min for anonymous, 10 req/min for authenticated users
+@ratelimit(key='ip', rate='5/m', block=True)
+def anonymous_view(request):
+    return JsonResponse({"message": "Anonymous view OK"})
+
+@ratelimit(key='ip', rate='10/m', block=True)
+def login_view(request):
+    return JsonResponse({"message": "Login allowed"})
